e02e640a98426290b1abf722a6e12285bb74fcb4
Pratham <dhamechapratham@gmail.com>
2024-04-12 10:05:15 +0530

    first commit

---
+++ force-app/main/default/lwc/accountLookupLWC/accountLookupLWC.js
@@ -0,0 +1,33 @@
+import { track} from 'lwc';
+import OmniscriptSelect from 'omnistudio/omniscriptSelect';
+import getAccount from '@salesforce/apex/GetAccount.getAccount';
+import { OmniscriptBaseMixin } from 'omnistudio/omniscriptBaseMixin';
+
+export default class accountLookupLWC extends OmniscriptBaseMixin(OmniscriptSelect) {
+
+   @track picklistdata = [];
+
+    connectedCallback() {
+        // console.log('in connected callback');
+        super.connectedCallback();
+        let contextId = this.jsonData.ContextId;
+        getAccount({ ContextId: contextId })
+            .then((result) => { 
+                console.log('result::',result);
+                console.log('result::',JSON.stringify(result));
+                result.forEach(currentItem => {
+                    this.picklistdata.push({
+                        value: currentItem.Id,
+                        label: currentItem.Name,
+                        data: currentItem
+                    });
+                });
+                this._realtimeOptions = this.picklistdata;
+            })
+            .catch((error) => {
+                console.error('error ::' + error);
+            });
+    }
+
+
+}
